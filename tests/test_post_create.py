96ef3be4e9babd2efb637fdaa7a2527ffd1bbd8a
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-05-08 16:46:25 +0300

    add test for create user

---
+++ tests/test_post_create.py
@@ -0,0 +1,17 @@
+from tests.utils import load_json
+import requests
+from jsonschema import validate
+
+def test_post_create_user():
+    url = "https://reqres.in/api/users"
+    payload = {
+        "name": "name",
+        "job": "job"
+    }
+
+    response = requests.post(url, data=payload)
+
+    assert response.status_code == 201
+    assert response.json()['name'] == 'name'
+    assert response.json()['job'] == 'job'
+    validate(response.json(), load_json('post_create_user.json'))
